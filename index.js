ebbbecd91975929d35b65230ede79231f78c6580
jatin-verma-1999 <jatin71_soe@jnu.ac.in>
2021-01-14 09:09:44 +0530

    Change template implemented

--- index.js
+++ index.js
@@ -4,17 +4,6 @@ const app=express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
-
-var array=[
-{head:"#F95700FF",body:"#00A4CCFF"},
-{head:"#00203FFF",body:"#ADEFD1FF"},
-{head:"#00539CFF",body:"#EEA47FFF"},
-{head:"#101820FF",body:"#FEE715FF"},
-{head:"#101820FF",body:"#F2AA4CFF"},
-{head:"#3C1053FF",body:"#DF6589FF"}
-];
-
-
 app.get('/',(req,res)=>{
 	res.sendFile(path.join(__dirname+'/views/main.html'));
 });
--- public/main.js
+++ public/main.js
@@ -1,2 +1,32 @@
+var array=[
+{head:"#F95700FF",body:"#00A4CCFF"},
+{head:"#00203FFF",body:"#ADEFD1FF"},
+{head:"#101820FF",body:"#FEE715FF"},
+{head:"#3C1053FF",body:"#DF6589FF"},
+{head:"#990011FF",body:"#FCF6F5FF"},
+{head:"#2C5F2DFF",body:"#FFE77AFF"}
+];
+
+document.querySelector('#ct').addEventListener('click',function(event){
+  var index=Math.floor(Math.random()*array.length);
+  var h=array[index].head;
+  var b=array[index].body;
 
 
+
+  document.querySelector('body').style.backgroundColor=b;
+  document.querySelector('html').style.backgroundColor=b;
+   document.querySelector('.navbar').style.backgroundColor=h;
+   document.querySelector('.navbar-brand').style.backgroundColor=b;
+  document.querySelector('.jumbotron').style.backgroundColor=h;
+  document.querySelector('.jumbotron').style.color=b;
+
+  document.querySelector('input').style.backgroundColor=b;
+  document.querySelectorAll('.card').forEach((item,index)=>{
+    item.style.backgroundColor=h;
+    item.style.color=b;
+  });
+  alert(h);
+
+
+});
